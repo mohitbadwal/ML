1a28c017f8663884fb116accab15bd345648ef59
mohitbadwal <badwalmohit@gmail.com>
2017-10-27 17:11:49 +0530

    ocr_line edited

--- python_files/ocr_line.py
+++ python_files/ocr_line.py
@@ -3,7 +3,7 @@ import string
 import pandas as pd
 from sklearn import model_selection
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.neighbors import KNeighborsClassifier
@@ -25,26 +25,30 @@ def cleanandstem(sentence):
     return cleaning(sentence)
 
 
-dataset = pd.read_csv(r'D:\backup\PycharmProjects\test\Image Batches-20171017T131547Z-001\Mohit_new.csv')
+dataset = pd.read_csv(r'D:\backup\PycharmProjects\test\Image Batches-20171017T131547Z-001\Not_Success_rows.csv',
+                      encoding='cp1256')
 s = ""
 is_remit_flag = 0
 is_total_flag = 0
 # replace yes to 1 and no to 0
-dataset['is_remittance'].replace("yes", 1, inplace=True)
-dataset['is_total'].replace("yes", 1, inplace=True)
-dataset['is_remittance'].replace("no", 0, inplace=True)
-dataset['is_total'].replace("no", 0, inplace=True)
+# dataset['is_remittance'].replace("yes", 1, inplace=True)
+# dataset['is_total'].replace("yes", 1, inplace=True)
+# dataset['is_remittance'].replace("no", 0, inplace=True)
+# dataset['is_total'].replace("no", 0, inplace=True)
 
-dataset = dataset[dataset['page_type'] == 'remittance']
-countVectorizer = CountVectorizer(tokenizer=cleanandstem, min_df=25, stop_words='english')
-theString = countVectorizer.fit_transform(dataset['row_string'])
+dataset = dataset[dataset['page_type_final'] == 'remittance']
+print(dataset.shape)
+# countVectorizer = CountVectorizer(tokenizer=cleanandstem, min_df=50,max_df=0.5, stop_words='english')
+# theString = countVectorizer.fit_transform(dataset['row_string'])
+tfidf = TfidfVectorizer(tokenizer=cleanandstem, min_df=50,max_df=0.4, stop_words='english')
+theString = tfidf.fit_transform(dataset['row_string'])
 combine1 = pd.DataFrame(theString.todense())
-combine1.columns = countVectorizer.get_feature_names()
+combine1.columns = tfidf.get_feature_names()
 print(combine1.columns)
 X = dataset.loc[:, ['check_noOfPages']]
 X = pd.concat([combine1.reset_index(drop=True), X.reset_index(drop=True)], axis=1, ignore_index=True)
-Y = dataset.loc[:, 'is_total']
-validation_size = 0.2
+Y = dataset.loc[:, 'is_total_final']
+validation_size = 0.3
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size)
 rfc = RandomForestClassifier(n_estimators=200)
 rfc.fit(X, Y)
@@ -52,3 +56,11 @@ predictions = rfc.predict(X_validation)
 print(accuracy_score(Y_validation, predictions))
 print(confusion_matrix(Y_validation, predictions))
 print(classification_report(Y_validation, predictions))
+
+
+
+'''
+
+([$]?[0-9]*[\,]?[0-9]*[\.]?[0-9]+)|((total)(s)?|(amount)).*([$]?([0-9]*[\,]?[0-9]*[\.]?[0-9]+))
+
+'''
