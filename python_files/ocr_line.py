63eed0cce164728b66c1be1f67df8ecedf97263d
mohitbadwal <badwalmohit@gmail.com>
2017-11-10 12:23:28 +0530

    push new line

--- python_files/ocr_line.py
+++ python_files/ocr_line.py
@@ -72,11 +72,11 @@ def isNumber(s, e):
     # print(len(er1),li)
     for j in range(0, len(er1)):
         li.pop(er1[j])
-    print(er,er1,i)
+    print(er, er1, i)
     if i == 1:
         f = 0
         for x in er:
-            print("printing",x, e)
+            print("printing", x, e)
             if convertNumber(x) == int(float(e)):
                 gh = x
                 # li = li[:f]
@@ -124,6 +124,8 @@ dataset = pd.read_csv(r'C:\Users\mohit.badwal.NOTEBOOK546.000\Downloads\Not_Succ
 s = ""
 is_remit_flag = 0
 is_total_flag = 0
+test_dataset = pd.read_csv(r'D:\backup\PycharmProjects\test\Image Batches-20171017T131547Z-001\python_files\test.csv',
+                           encoding='cp1256')
 
 
 # replace yes to 1 and no to 0
@@ -150,15 +152,33 @@ print(dataset.shape)
 # theString = countVectorizer.fit_transform(dataset['row_string'])
 dataset['total'] = dataset.apply(totalFlag, axis=1)
 dataset = last(dataset)
-tfidf = TfidfVectorizer(tokenizer=cleanandstem, min_df=100, stop_words='english', vocabulary=
+test_dataset['total'] = test_dataset.apply(totalFlag, axis=1)
+test_dataset = last(test_dataset)
+tfidf = CountVectorizer(tokenizer=cleanandstem, min_df=5, stop_words='english', vocabulary=
 {'total', 'totals', 'grand', 'check', 'date', 'paid', 'net'})
 theString = tfidf.fit_transform(dataset['row_string'])
+testString = tfidf.transform(test_dataset['row_string'])
 combine1 = pd.DataFrame(theString.todense())
 combine1.columns = tfidf.get_feature_names()
+combine2 = pd.DataFrame(testString.todense())
+combine2.columns = tfidf.get_feature_names()
 print(combine1.columns)
 X = dataset.loc[:, ['total', 'row_isLastRow', 'row_string']]
 X = pd.concat([combine1.reset_index(drop=True), X.reset_index(drop=True)], axis=1, ignore_index=True)
 Y = dataset.loc[:, 'is_total_final']
+X = X.iloc[:, :-1]
+X1 = test_dataset.loc[:, ['total',
+                          'row_isLastRow',
+                          ]]
+X1 = pd.concat([combine2.reset_index(drop=True), X1.reset_index(drop=True)], axis=1, ignore_index=True)
+rfc = RandomForestClassifier(n_estimators=200, )
+rfc.fit(X, Y)
+predictions = rfc.predict(X1)
+predictions_prob = rfc.predict_proba(X1)
+test_dataset['is_total'] = pd.DataFrame(data=predictions)
+test_dataset.to_csv("test.csv")
+
+'''
 validation_size = 0.2
 seed = 20
 X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
@@ -202,7 +222,7 @@ print(confusion_matrix(Y, X.iloc[:, -2].values))
 print(classification_report(Y, X.iloc[:, -2].values))
 dataset[dataset['is_total_final'] != dataset['total']].loc[:, ['row_index', 'row_string', 'is_total_final', 'total']] \
     .to_csv('ocr_no_match.csv')
-
+'''
 '''
 ([$]?[0-9]*[\,]?[0-9]*[\.]?[0-9]+)|(.*((total)(s)?|(amount)).*([$]?([0-9]*[\,]?[0-9]*[\.]?[0-9]+)))
 '''
